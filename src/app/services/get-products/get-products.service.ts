e77d76acc85187998d7e5d1d1fe6bf50b4800639
Igor Martins <igor.rocha@nees.ufal.br>
2024-05-28 14:22:32 -0300

    adjusted paths

--- src/app/services/get-products/get-products.service.ts
+++ src/app/services/get-products/get-products.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
-import { Product } from '../models/product.model';
+import { Product } from '../../models/product.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
